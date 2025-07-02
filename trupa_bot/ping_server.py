495a95faf22aebac86441687d6bee769040a46a4
Brent Justine Barbadillo <168087779+brentjustine@users.noreply.github.com>
2025-07-02 21:38:46 +0800

    Update ping_server.py

--- trupa_bot/ping_server.py
+++ trupa_bot/ping_server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import threading
+import os
 import main  # this runs your trading bot
 
 app = Flask(__name__)
@@ -9,7 +10,9 @@ def home():
     return "✅ Bot is alive."
 
 if __name__ == '__main__':
-    # Run your bot in a background thread
+    # Run bot in background thread
     threading.Thread(target=main.main).start()
-    # Start the web server
-    app.run(host='0.0.0.0', port=10000)
+
+    # Run Flask web server for Render to ping
+    PORT = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=PORT)
